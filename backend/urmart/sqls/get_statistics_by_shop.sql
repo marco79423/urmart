58658b433eb0aa481282c15736258a538b49b9d7
marco79423 <marco79423@gmail.com>
2019-12-28 01:08:50 +0800

    Fix sql bug

--- backend/urmart/sqls/get_statistics_by_shop.sql
+++ backend/urmart/sqls/get_statistics_by_shop.sql
@@ -1,7 +1,7 @@
-SELECT id,
-       name,
-       SUM(total_price) as total_price,
-       SUM(qty)         as total_qty,
+SELECT uo.id,
+       uo.name,
+       SUM(uo.total_price) as total_price,
+       SUM(uo.qty)         as total_qty,
        count(1)         as order_qty
 FROM (SELECT urmart_shop.id,
              urmart_shop.name,
@@ -9,5 +9,5 @@ FROM (SELECT urmart_shop.id,
              (qty * urmart_order.price) as total_price
       FROM urmart_order
                INNER JOIN urmart_product ON product_id = urmart_product.id
-               INNER JOIN urmart_shop ON shop_id = urmart_shop.id)
-GROUP BY name;
+               INNER JOIN urmart_shop ON shop_id = urmart_shop.id) AS uo
+GROUP BY uo.id, uo.name;
